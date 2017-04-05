3f38aa9c160674a824571f5d2145c1d045cead36
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-04-05 16:31:55 +0200

    caracteres_aleatoires

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ function chaine( nbCaracteres ){
  	var caracteres = "abcdefghijklmnopqrstuvwxyz0123456789";
 
  	for( var i=0; i < nbCaracteres; i++ ){
+
         text += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
  	}
-	console.log(text);
+	//console.log(text);
  	return text;       
 
 }
  
-chaine(17);
- 
+function tableaujs (nblignes, nbcolones){
+	var tableau = [];
+	
+	for( var i=0 ; i<nblignes ; i++ ){	    
+	tableau.push( [] );
+
+		for( var j=0 ; j<nbcolones ; j++ ){ 
+		tableau[i].push( j );
+		}
+	}
+
+	console.log(tableau);
+}
+
+chaine(10);
+
+tableaujs(5, 4);
